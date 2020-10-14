1e4056063d4fdebf183399f2f9b0fab0f4abb8ef
Michael Wade <mawade21@gmail.com>
2020-10-13 20:16:55 -0600

    it fucking works

--- pycewl.py
+++ pycewl.py
@@ -1,5 +1,6 @@
 import requests
 import time
+import re
 from bs4 import BeautifulSoup
 
 
@@ -10,6 +11,7 @@ authorlist = []
 
 depth = 2
 
+#TODO: Use the click package to take in arguments
 url = "https://www.duanewaddle.com/"
 
 linklist.append(url)
@@ -18,12 +20,29 @@ def AddLinkToLinkList(url, linklist):
     if(url in linklist):
         print(f"URL: {url} already in list")
     else:
-        linklist.append(url)
-        print(f"URL: {url} added to list")
+        urlRoot = url.split("/")
+        domainRoot = linklist[0].split("/")
+        if(urlRoot[2] == domainRoot[2]):
+            linklist.append(url)
+            print(f"URL: {url} added to list")
+        else:
+            print(f"URL: {url} is not in scope")
     return linklist
 
 def AddToWordList(word, wordlist):
-    pass
+    print(word)
+    words = word.split(" ")
+    for w in words:
+        x = re.search("[a-zA_Z]{4,}", w)
+        #TODO: Remove Special Characters
+        if x:
+            if(w in wordlist):
+                print(f"Word: {w} already in list")
+            else:
+                wordlist.append(w)
+        else:
+            print(f"Word: {w} is not a word")
+    return wordlist
 
 def AddEmailtoEmailList(email, emaillist):
     pass
@@ -33,6 +52,17 @@ def AddAuthortoAuthorList(author, authorlist):
 
 def ParsePage(url, wordlist, emaillist, authorlist):
     print(f"Page {url} is being scanned")
+    try:
+        req = requests.get(url)
+        soup = BeautifulSoup(req.text, 'html.parser')
+        for text in soup.stripped_strings:
+            #TODO: Check if its an email
+            #TODO: Check if its a document
+            wordlist = AddToWordList(text, wordlist)
+        return wordlist
+    except:
+        #TODO: Better Error messages
+        print("Error 3")
 
 def GetLinks(url, linklist):
     try:
@@ -53,19 +83,21 @@ def GetLinks(url, linklist):
 
 
 if __name__ == "__main__":
-    print(linklist)
-    counter = 0
-    while counter < depth:
-        counter = counter + 1
-        print(f"Counter at {counter}")
-        time.sleep(3)
-        for url in linklist:
-            linklist = GetLinks(url, linklist)
+    #TODO: Allow for several levels of recursion 
+    linklist = GetLinks(url, linklist)
+    
+    for site in linklist:
+        ParsePage(site, wordlist, emaillist, authorlist)
+    print(wordlist)
+    
 
 
    
 
-
+#General TODOs
+#TODO: Create a log option
+#TODO: Dress up the messaging back to the user
+#TODO: Async
 
 
 
